f47e966a33818abbebe64548524f9af3f525fcec
Spencer Hubert <spencerhhubert@gmail.com>
2024-08-26 11:36:25 -0400

    i hate whitespace

--- compare.js
+++ compare.js
@@ -1,31 +1,13 @@
 function cosineSimilarity(a, b) {
-  console.log("cosineSimilarity", a, b);
-  if (!a) {
-    throw new Error("First array is empty");
-  }
-  if (!b) {
-    throw new Error("Second array is empty");
-  }
-  if (a.length !== b.length) {
-    throw new Error("Arrays must have the same length");
-  }
-
-  let dotProduct = 0;
-  let normA = 0;
-  let normB = 0;
-
+  if (a.length !== b.length) throw new Error("Arrays must have the same length");
+  let dot = 0, normA = 0, normB = 0;
   for (let i = 0; i < a.length; i++) {
-    dotProduct += a[i] * b[i];
+    dot += a[i] * b[i];
     normA += a[i] * a[i];
     normB += b[i] * b[i];
   }
-
   normA = Math.sqrt(normA);
   normB = Math.sqrt(normB);
-
-  if (normA === 0 || normB === 0) {
-    return 0;
-  }
-
+  if (normA === 0 || normB === 0) return 0;
   return dotProduct / (normA * normB);
 }
